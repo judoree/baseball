14c6066b9e30d1f732fe4d3b9777fdaef4ff0174
김주현 <judoree9629@gmail.com>
2026-03-09 17:17:56 +0900

    feat: initial commit

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,3 @@
 import { type RouteConfig, index, route } from '@react-router/dev/routes';
 
-export default [
-  index('routes/home.tsx'),
-  route('/player', 'player/play.tsx'),
-] satisfies RouteConfig;
+export default [index('routes/home.tsx')] satisfies RouteConfig;
