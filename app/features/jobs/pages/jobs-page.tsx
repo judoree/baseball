180205b63209167a2b1d668572f2456c1f3369f0
김주현 <judoree9629@gmail.com>
2026-05-13 18:30:18 +0900

    fix : job detail page

--- app/features/jobs/pages/jobs-page.tsx
+++ app/features/jobs/pages/jobs-page.tsx
@@ -27,7 +27,7 @@ export default function JobsPage() {
       <Hero title="Jobs" subtitle="Companies looking for makers" />
       <div className="grid grid-cols-6 gap-20 items-start">
         <div className="grid grid-cols-3 col-span-4 gap-5">
-          {Array.from({ length: 10 }).map((_, index) => (
+          {Array.from({ length: 20 }).map((_, index) => (
             <JobCard
               key={`jobid-${index}`}
               to={`jobid-${index}`}
@@ -43,8 +43,9 @@ export default function JobsPage() {
             />
           ))}
         </div>
-        <div className="col-span-2 flex flex-col gap-10">
+        <div className="col-span-2 flex sticky top-20 flex-col gap-10">
           <div className="flex flex-col gap-2.5">
+            ``
             <h4 className="text-sm text-muted-foreground font-bold">Type</h4>
             <div className="flex flex-wrap gap-2">
               {JOB_TYPES.map((type) => (
